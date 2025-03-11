c5f806fe8fa90ae5602d2399cf5a14f815039c50
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-03-11 22:20:28 +0300

    Added different types of functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(param1 = "*") {
+    return function (param2 = "special") {
+        return `You are ${param1}${param2}${param1}!`;
+    };
+}
+
+console.log(wrapAdjective("%")("a dedicated programmer"))
